fd657bb14b705f7f2385ff4ad1b01c574ded07be
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-06-22 21:46:05 +0700

    feat: add table & row constructors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use colored::Colorize;
+
+use crate::row::Row;
+use crate::table::Table;
+
+mod row;
+mod table;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}{}", "Bubble".bold(), "DB".bold().bright_magenta());
+
+    let mut a = Table::new("a");
+    let b = Row::zeros("b", "Hello world!");
+    let c = Row::zeros("c", "I am Nguyen");
+
+    a.add(b);
+    a.add(c);
+
+    match a.get("a") {
+        Some(r) => println!("{}", r.value),
+        None => println!("Not found"),
+    }
+
+    match a.get("b") {
+        Some(r) => println!("{}", r.value),
+        None => println!("Not found"),
+    }
+
+    match a.get("c") {
+        Some(r) => println!("{}", r.value),
+        None => println!("Not found"),
+    }
 }
--- src/table.rs
+++ src/table.rs
@@ -1,1 +1,31 @@
 // Manage: rows
+
+use crate::row::Row;
+
+pub struct Table {
+    id: String,
+    rows: Option<Vec<Row>>,
+}
+
+impl Table {
+    pub fn new(id: &str) -> Self {
+        Table {
+            id: id.to_string(),
+            rows: None,
+        }
+    }
+
+    pub fn add(&mut self, r: Row) {
+        match &mut self.rows {
+            Some(rows) => rows.push(r),
+            None => self.rows = Some(vec![r]),
+        }
+    }
+
+    pub fn get(&self, k: &str) -> Option<&Row> {
+        match &self.rows {
+            Some(rows) => rows.iter().find(|&x| x.k == k),
+            None => None,
+        }
+    }
+}
