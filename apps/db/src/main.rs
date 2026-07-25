4ffa02be36ae3bc75b6890e15bde08fdb923675c
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-07-25 13:36:09 +0700

    feat(db): done kvec

--- apps/db/src/main.rs
+++ apps/db/src/main.rs
@@ -1,17 +1,38 @@
 #[macro_use]
 extern crate rocket;
 
-use kv::Store;
+use kv::{KVal, KVec};
 use rocket::State;
+use rocket::serde::json::Json;
+use serde::{Deserialize, Serialize};
 
-#[post("/kv/<key>/<value>")]
-fn set(key: String, value: String, store: &State<Store>) -> String {
+#[derive(Serialize, Deserialize)]
+struct KVecReqBody {
+    data: Vec<f64>,
+}
+
+#[post("/kvec/<key>", format = "json", data = "<req>")]
+fn set_vector(key: String, req: Json<KVecReqBody>, vec_store: &State<KVec>) -> String {
+    vec_store.set(key, req.data.clone());
+    "Vector set successfuly".to_string()
+}
+
+#[get("/kvec/<key>")]
+fn get_vector(key: String, vec_store: &State<KVec>) -> String {
+    match vec_store.get(&key) {
+        Some(vec) => serde_json::to_string(&vec).unwrap(),
+        None => "Vector not found".to_string(),
+    }
+}
+
+#[post("/kval/<key>/<value>")]
+fn set(key: String, value: String, store: &State<KVal>) -> String {
     store.set(key, value);
     "Value set successfuly".to_string()
 }
 
-#[get("/kv/<key>")]
-fn get(key: String, store: &State<Store>) -> String {
+#[get("/kval/<key>")]
+fn get(key: String, store: &State<KVal>) -> String {
     match store.get(&key) {
         Some(value) => value,
         None => "Key not found".to_string(),
@@ -21,6 +42,7 @@ fn get(key: String, store: &State<Store>) -> String {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .manage(Store::new())
-        .mount("/", routes![set, get])
+        .manage(KVal::new())
+        .manage(KVec::new())
+        .mount("/", routes![set, get, set_vector, get_vector])
 }
--- libs/kv/src/lib.rs
+++ libs/kv/src/lib.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
 use std::sync::Mutex;
-pub struct Store {
+pub struct KVal {
     data: Mutex<HashMap<String, String>>,
 }
 
-impl Store {
+impl KVal {
     pub fn new() -> Self {
-        Store {
+        KVal {
             data: Mutex::new(HashMap::new()),
         }
     }
@@ -16,6 +16,28 @@ impl Store {
     }
 
     pub fn set(&self, k: String, v: String) {
+        if let Ok(mut data) = self.data.lock() {
+            data.insert(k, v);
+        }
+    }
+}
+
+pub struct KVec {
+    data: Mutex<HashMap<String, Vec<f64>>>,
+}
+
+impl KVec {
+    pub fn new() -> Self {
+        KVec {
+            data: Mutex::new(HashMap::new()),
+        }
+    }
+
+    pub fn get(&self, k: &str) -> Option<Vec<f64>> {
+        self.data.lock().ok().and_then(|data| data.get(k).cloned())
+    }
+
+    pub fn set(&self, k: String, v: Vec<f64>) {
         if let Ok(mut data) = self.data.lock() {
             data.insert(k, v);
         }
