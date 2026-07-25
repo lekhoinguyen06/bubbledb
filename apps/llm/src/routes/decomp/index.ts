a3ec6de407a54e3ef8b6efffca140844b21e875d
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-07-25 11:24:04 +0700

    refactor(llm/decomp): modulize project structure

--- apps/llm/src/routes/decomp/index.ts
+++ apps/llm/src/routes/decomp/index.ts
@@ -3,26 +3,8 @@ import { DecompReqBodyDTO, DecompResBodyDTO } from "./decomp.dto";
 import { generateText, Output } from "ai";
 import { ollama } from "ai-sdk-ollama";
 import { type } from "arktype";
+import { decompHandler } from "./decomp.service";
 
 export const decompRoutes = new Hono()
 
-decompRoutes.post('/', async (c) => {
-  const json = await c.req.json()
-  const body = DecompReqBodyDTO(json)
-
-  if (body instanceof type.errors) {
-    return c.json({ error: body.summary }, 400)
-  }
-
-  const { usage, output } = await generateText({
-    model: ollama('gemma3:1b'),
-    prompt: `Decompose the following sentence into subject, relation, and object: ${body.sentence}`,
-    temperature: body.temp,
-    maxOutputTokens: body.maxOutputTokens,
-    output: Output.object({
-      schema: DecompResBodyDTO,
-    })
-  })
-
-  return c.json({ output, usage })
-})
+decompRoutes.post('/', decompHandler)
